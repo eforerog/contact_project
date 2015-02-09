02516160c1b12ac33ccd114e15e3f7fd80c5eb47
Edgar Forero <edgar@toro-labs.com>
2015-02-09 18:03:11 -0500

    validation data

--- contact_app/app/Http/Controllers/ContactsController.php
+++ contact_app/app/Http/Controllers/ContactsController.php
@@ -7,7 +7,7 @@ use Validator;
 use App\Contact;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
-
+use DateTime;
 use Excel;
 
 use Illuminate\Http\Request;
@@ -127,10 +127,10 @@ class ContactsController extends Controller {
 	}
 	
 	/**
-	 * Process data.
+	 * form to upload contact data.
 	 *
 	 * 
-	 * @return Response
+	 * @return view
 	 */
 	 
 	public function loadSpreadsheet()
@@ -138,9 +138,16 @@ class ContactsController extends Controller {
 		return view('contacts.loadsheet');
 	}
 	 
+	 /**
+	 * Process contact data.
+	 *
+	 * 
+	 * @return view
+	 */
 	 
 	public function importSpreadsheet()
 	{
+		$dateformat = "";
 		if (Input::hasFile('file_data')) {
 			
 			Input::file('file_data')->move('public/uploads', Input::file('file_data')->getClientOriginalName());
@@ -148,20 +155,92 @@ class ContactsController extends Controller {
 			switch(Input::file('file_data')->guessClientExtension())
 			{
 				case "xls":	break;
-				case "xlsx":	break;
+				case "xlsx":break;
 				case "csv":	$xx = Excel::setDelimiter(";");
 							break;
 				case "tsv": 	$xx = Excel::setDelimiter("\t");
 							break;
-				default:
-							break;
+				default:		break;
 			}
 			
 			$url = 'public/uploads/' . Input::file('file_data')->getClientOriginalName();
 			
 			$xx = Excel::load($url, function($reader) {
 				
-				echo $reader->first();
+				$rows = $reader->all();
+				$first_row = $reader->first();
+				
+				//headers check
+				$test_header =  json_encode($first_row);
+				
+				if ((strpos($test_header,'"first_name"') !== false) 
+					&& (strpos($test_header,'"last_name"') !== false)
+					&& (strpos($test_header,'"gender"') !== false)
+					&& (strpos($test_header,'"birthday"') !== false)
+					&& (strpos($test_header,'"emails"') !== false)
+					&& (strpos($test_header,'"phones"') !== false)){
+						
+						$count_error = 0;
+						$error_message = "";
+						foreach ($rows as $key=>$row ) {
+							//verifiy required data
+							if(!($row->first_name != "" && $row->last_name !="" && $row->phones != "")) {
+								$error_message = $error_message . "required info missing at row " . ($key + 1) . "<br />";
+								$count_error+=1;
+							}
+							//verify gender format
+							if(!($row->gender == "M" || $row->gender == "F" || $row->gender == "") ){
+								$error_message = $error_message . "gender format error at row " . ($key + 1) . "<br />";	
+								$count_error+=1;
+							}
+							
+							//verify birthday format
+							switch(Input::file('file_data')->guessClientExtension())
+							{
+								case "xls":	$dateformat = 'Y-m-d H:i:s';
+											break;
+								case "xlsx":$dateformat = 'Y-m-d H:i:s';
+											break;
+								case "csv":	$dateformat = 'd/m/Y';
+											break;
+								case "tsv": 	$dateformat = 'd/m/Y';
+											break;
+								default:
+											break;
+							}
+							
+							$ymd = DateTime::createFromFormat($dateformat, $row->birthday);
+							if(!$ymd){
+								$error_message = $error_message . "invalid date format at row : " . ($key + 1) . "<br />";
+								$count_error+=1;
+							}
+							
+						}
+						
+						if ($count_error > 0){
+							echo "hay errores en el archivo: " . $error_message;
+						}
+						else{
+								
+						}
+						
+				}
+				else
+				{
+					echo "bad headers";
+				}
+				
+				/*if (isset($firstrow['first_name']) && isset($firstrow['last_name']) && isset($firstrow['gender']) && isset($firstrow['birthday']) && isset($firstrow['phones']) && isset($firstrow['emails'])) {
+					echo "completo";
+				}
+				
+				echo $reader->first()->first_name;
+				echo $reader->first()->last_name;
+				echo $reader->first()->gender;
+				echo $reader->first()->birthday;
+				echo $reader->first()->emails;
+				echo $reader->first()->phones;
+				*/
 			})->get();
 			
 			
